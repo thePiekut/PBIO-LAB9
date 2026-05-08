db1a81e0e4025a430735a8d74474c8a695bdf0e1
thePiekut <p.m.piekut@gmail.com>
2026-05-08 13:40:18 +0200

    Added transcribtion to mRNA and GC content plot generator

--- s29782_2026.py
+++ s29782_2026.py
@@ -1,5 +1,6 @@
 import random
 from collections import defaultdict
+import matplotlib.pyplot as plt
 
 def generate_sequence(length: int) -> str:
     """Zwraca losową sekwencję DNA o zadanej długości."""
@@ -81,8 +82,30 @@ def pretty_print_stats(stats :dict):
     print(f"\nGC-content: {round(stats["GC"]*100,3)}%")
 
 
+def transcribe_to_mrna(sequence: str) -> str:
+    return sequence.upper().replace("T", "U")
 
 
+def plot_gc(seq: str ,output_file:str):
+    window_size=len(seq)//10
+    gc_content=[]
+    for i in range(len(seq) - window_size + 1):
+        window = seq[i : i + window_size]
+        gc_count = window.count('G') + window.count('C')
+        gc_content.append(round((gc_count / window_size) * 100,3))
+    
+    x_positions = [i + (window_size // 2) for i in range(len(gc_content))]
+    plt.figure(figsize=(10, 5))
+    plt.plot(x_positions, gc_content, label='GC content')
+    plt.xlabel("Pozycja w sekwencji")
+    plt.ylabel("GC %")
+    plt.legend()
+    plt.grid(True, linestyle='--', alpha=0.7)
+    plt.savefig(output_file)
+    plt.close()
+    print(f"Wykres zapisany w {output_file}")
+
+    
 
 
 
@@ -92,10 +115,28 @@ def main():
     seq_id=get_ID()
     seq=generate_sequence(seq_len)
     seq_w_name=insert_name(seq,input("Podaj imie: "))
-    print(format_fasta(seq_id,"bez",seq_w_name))
-    save_to_fasta(seq_id,format_fasta(seq_id,"bez",seq_w_name))
+    description=input("Podaj opis: ")
+
+    fasta=format_fasta(seq_id,description,seq_w_name)
+
+    print(fasta)
+
+    save_to_fasta(seq_id,fasta)
+    
+
+    if input("Czy wykonać transkrypcję do mRNA? (y/n): ").lower() == 'y':
+        mrna = transcribe_to_mrna(seq)
+        mFasta=format_fasta(f"{seq_id}_mRNA", "Transkrypt mRNA", mrna)
+        save_to_fasta(f"{seq_id}_mRNA",mFasta)
+    
+    if input("Czy wygenerować wykres GC? (y/n): ").lower() == 'y':
+        plot_gc(seq,f"{seq_id}_plot.png")
+
+    print()
     stats=calculate_stats(seq)
     pretty_print_stats(stats)
+
+
 
 
    
